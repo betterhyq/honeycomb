775de8adafe0f50768c0905de0c7b5ff6dc2dc43
hyq <betterhyq@qq.com>
2026-01-18 21:53:19 +0800

    feat: 精细化页面

--- packages/honeycomb-client/src/App.vue
+++ packages/honeycomb-client/src/App.vue
@@ -153,8 +153,15 @@ onMounted(() => {
   <el-watermark content="honeycomb">
     <el-container>
 		<div style="width: 100%; height: 60px;"></div>
-		<el-header style="display: flex; align-items: center; border-bottom: 1px solid var(--el-border-color); position: fixed; top: 0; left: 0; right: 0; z-index: 1000; background-color: var(--el-bg-color);">
+		<el-header style="display: flex; align-items: center; justify-content: space-between; border-bottom: 1px solid var(--el-border-color); position: fixed; top: 0; left: 0; right: 0; z-index: 1000; background-color: var(--el-bg-color);">
 			<img class="logo-img" src="/logo.svg" alt="Honeycomb Logo" />
+			<el-space>
+				<el-link href="https://github.com/betterhyq/honeycomb" target="_blank" underline="never">
+					<el-icon :size="24">
+						<svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29.3444 30.4765C31.7481 29.977 33.9292 29.1108 35.6247 27.8391C38.5202 25.6676 40 22.3136 40 18.9999C40 16.6752 39.1187 14.505 37.5929 12.6668C36.7427 11.6425 39.2295 3.99989 37.02 5.02919C34.8105 6.05848 31.5708 8.33679 29.8726 7.83398C28.0545 7.29565 26.0733 6.99989 24 6.99989C22.1992 6.99989 20.4679 7.22301 18.8526 7.6344C16.5046 8.23237 14.2591 5.99989 12 5.02919C9.74086 4.05848 10.9736 11.9632 10.3026 12.7944C8.84119 14.6051 8 16.7288 8 18.9999C8 22.3136 9.79086 25.6676 12.6863 27.8391C14.6151 29.2857 17.034 30.2076 19.7401 30.6619" stroke="#000" stroke-width="4" stroke-linecap="round"/><path d="M19.7397 30.6619C18.5812 31.937 18.002 33.1478 18.002 34.2944C18.002 35.441 18.002 38.3464 18.002 43.0106" stroke="#000" stroke-width="4" stroke-linecap="round"/><path d="M29.3446 30.4766C30.4423 31.9174 30.9912 33.211 30.9912 34.3576C30.9912 35.5042 30.9912 38.3885 30.9912 43.0107" stroke="#000" stroke-width="4" stroke-linecap="round"/><path d="M6 31.2155C6.89887 31.3254 7.56554 31.7387 8 32.4554C8.65169 33.5303 11.0742 37.518 13.8251 37.518C15.6591 37.518 17.0515 37.518 18.0024 37.518" stroke="#000" stroke-width="4" stroke-linecap="round"/></svg>
+					</el-icon>
+				</el-link>
+			</el-space>
 		</el-header>
       <el-main>
         <!-- 统计信息卡片 -->
